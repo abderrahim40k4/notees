b179d773ad1eb2be56c7e7a4eb718362ba8c672d
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-18 14:45:01 +0200

    init

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='font-roboto'>
+      app
+      <h4 className="font-syne">hello world</h4>
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// Supports weights 100-900
+import '@fontsource-variable/roboto-condensed';
+// Supports weights 400-800
+import '@fontsource-variable/syne';
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
