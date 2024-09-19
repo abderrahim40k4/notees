b5e43d53c73fbe7bbe9cdd3dbb905dfe2360834e
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-19 20:54:42 +0200

    validatiom healper

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import { Link } from 'react-router-dom'
 import InputPassword from '../../components/inputs/InputPassword'
+import { validateEmail } from '../../utils/helper'
 
 const Home = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(null)
+    const handelLogin = async (e)=>{
+        e.preventDefault();
+        if (!validateEmail(email)) {
+            setError("please Eneter a Valid Email address")
+            return;
+        }
+
+    }
   return (
    <>
     <Navbar/>
@@ -12,10 +24,11 @@ const Home = () => {
         <div className="w-96 border rounded bg-white px-7 py-10">
             <form action="" onSubmit={()=>{}}>
                 <h4 className="text-2xl mb-7 font-syne font-semibold">Log in </h4>
-                <input type="email" name="Email" id="email" placeholder='Email' className='input-box' />
-                <InputPassword />
+                <input type="email" name="Email" id="email" placeholder='Email' className='input-box' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+                <InputPassword value={password} onchange={(e)=>{setPassword(e.target.value)}}/>
                 <button type="submit" className="btn-primary" >Log in</button>
-                <p className='text-sm text-center mt-4 font-syne'>
+                <p className='text-sm text-center mt-4 font-syne' onClick={handelLogin()}>
                     you don't have acou yet ?{""}
                     <Link to={"/sign-up"} className='font-medium text-primary underline font-syne'>Creat Acount</Link>
                 </p>
---
+++ front-end/src/utils/helper.js
@@ -0,0 +1,4 @@
+export const validateEmail = (email) =>{
+    const regex = "name";
+    return regex.test(email);
+}
