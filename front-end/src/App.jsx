3c3c3fb1856c8c6b2bf6ce1333b47ef87b478e09
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-19 17:48:45 +0200

    router created

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import * as ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+}
+ from "react-router-dom";
+import Login from './pages/login/Login.jsx';
+import Signup from './pages/signup/Signup.jsx'
+import Home from './pages/home/Home.jsx';
+
+
 const App = () => {
+  const routes = (
+    <Router>
+      <Routes>
+          <Route path='/dashboard' element={<Home/>}/>
+          <Route path='/log-in' exact element={<Login/>}/>
+          <Route path='/sign-up' exact element={<Signup/>}/>
+      </Routes>
+    </Router>
+  )
   return (
     <div className='font-roboto'>
-      app
-      <h4 className="font-syne">hello world</h4>
+      {routes}
+      
     </div>
   )
 }
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from './pages/home/Home.jsx';
 // Supports weights 100-900
 import '@fontsource-variable/roboto-condensed';
 // Supports weights 400-800
