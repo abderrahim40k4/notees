93b630908810cb3d2c97084b860af1ef18351186
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-20 15:00:49 +0200

    ui Dashbourd

---
+++ front-end/src/components/cards/NoteCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { MdCreate, MdDelete, MdOutlinePushPin } from 'react-icons/md'
+
+const NoteCard = ({ title, date, content, tags, isPinnd, onEdit, onDelete, onPinNote }) => {
+    return (
+        <div className="border rounded hover:shadow-xl p-4 bg-white transition-all ease-in-out">
+            <div className="flex items-center justify-between">
+                <div className="">
+                    <h6 className="text-sm font-syne font-medium ">{title}</h6>
+                    <span className="text-xs text-slate-500">{date}</span>
+                </div>
+                <MdOutlinePushPin className={`icon-btn ${isPinnd ? 'text-primary' : 'text-slate-600'}`} onClick={onPinNote} />
+            </div>
+            <p className=''>{content?.slice(0, 60)}</p>
+            <div className="flex justify-between items-center ">
+                <div className="text-sm text-slate-500 font-syne">{tags}</div>
+
+                <div className="flex items-center gap-2">
+                    <MdCreate
+                        className='icon-btn hover:text-green-500'
+                        onClick={onEdit}
+                    />
+                    <MdDelete
+                        className='icon-btn hover:text-red-500'
+                        onClick={onDelete}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NoteCard
--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
-import { Link } from 'react-router-dom'
-import InputPassword from '../../components/inputs/InputPassword'
-import { validateEmail } from '../../utils/helper'
-import PfInfo from '../../components/cards/PfInfo'
+import NoteCard from '../../components/cards/NoteCard'
+import {MdAdd} from "react-icons/md"
 
 const Home = () => {
     return (
         <>
             <Navbar/>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-4 mt-8">
+                <NoteCard 
+                title="coding on today " 
+                date="20-09-2024" 
+                tags="#coding" 
+                content="coding all day long from wekin up till slip with max possible "
+                isPinnd={true}
+                onDelete={()=>{}}
+                onEdit={()=>{}}
+                onPinNote={()=>{}}
+                />
+                </div>
+            </div>
+            <button className="w-16 h-16 flex items-center justify-center bg-primary hover:bg-blue-700 rounded-xl right-10 bottom-10 absolute" onClick={()=>{}}>
+                <MdAdd className="text-[32px] text-white"/>
+            </button>
         </>
     )
 }
