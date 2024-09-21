55453ec1054866379fe23ba548ae9e757b0874aa
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-09-21 12:16:59 +0200

    tags update

--- front-end/src/components/inputs/TagInput.jsx
+++ front-end/src/components/inputs/TagInput.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
-import { MdAdd } from 'react-icons/md'
+import React, { useState } from 'react'
+import { MdAdd, MdClose } from 'react-icons/md'
 
-const TagInput = ({tags, setTags}) => {
-  return (
-    <div>
-        <div className="flex items-center gap-4 mt-3">
-            <input type="text" className="text-sm bg-transparent border px-3 py-2 rounded outline-none" placeholder='Add Tags'/>
-            <button className="w-8 h-8 flex items-center justify-center rounded border border-primary hover:bg-primary">
-                <MdAdd className='text-2xl text-primary hover:text-white'/>
-            </button>
+const TagInput = ({ tags, setTags }) => {
+
+    const [inputValue, setInputValue] = useState("");
+    const handelInput = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const addNewTag = () => {
+        if (inputValue.trim() !== "") {
+            setTags([...tags, inputValue.trim()]);
+            setInputValue("")
+
+        }
+    };
+    const handelKeyDown = (e) => {
+        if (e.key === "Enter") {
+            addNewTag()
+        }
+    }
+
+    const handellRemoveTag=(remvedTag)=>{
+        setTags(tags.filter((tag)=>tag !== remvedTag))
+    }
+
+    return (
+        <div>
+
+            <div className="">
+                {
+                    tags?.lengt > 0 && <div className=''>
+                        {tags.map((tag, index) => (
+                            <span key={index}>{tag}
+
+                                <button className='' onClick={() => { handellRemoveTag(tag) }}>
+                                    <MdClose className='' />
+                                </button>
+                            </span>
+                        ))}
+                    </div>
+                }
+            </div>
+            <div className="flex items-center gap-4 mt-3">
+                <input type="text"
+                    className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
+                    placeholder='Add Tags'
+                    onChange={handelInput}
+                    onKeyDown={handelKeyDown} />
+                <button className="w-8 h-8 flex items-center justify-center rounded border border-primary hover:bg-primary" onClick={() => { addNewTag() }} >
+                    <MdAdd className='text-2xl text-primary hover:text-white'/>
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default TagInput
--- front-end/src/pages/home/AddNotes.jsx
+++ front-end/src/pages/home/AddNotes.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import TagInput from '../../components/inputs/TagInput'
 
 const AddNotes = () => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [tags, setTags] = useState([])
+
+
   return (
     <div>
         <div className="flex flex-col gap-2 ">
         <label htmlFor="" className='input-label'>enter a note Title</label>
         <input type="text"
         placeholder="Coding at 8AM"
-        className='text-2xl text-slate-900 outline-none' 
+        className='text-2xl text-slate-900 outline-none'
+        title={title}
+        onChange={({target})=>{setTitle(target.value)}} 
         />
 
         </div>
@@ -18,12 +26,15 @@ const AddNotes = () => {
         <textarea type="text"
         className='text-sm text-slate-900 outline-none bg-slate-200 p-2 rounded'
         placeholder='content...' 
-        rows={10}/>
+        rows={10}
+        value={content}
+        onChange={({target})=>{setContent(target.value)}}
+        />
         </div>
 
         <div className="mt-3">
         <label htmlFor="" className='input-label'>#Tags</label>
-        <TagInput/>
+        <TagInput tags={tags} setTags={setTags}/>
         </div>
        <button className="btn-primary font-medium mt-5 p-3">ADD</button>
     </div>
